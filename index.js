1cbb0a5baf90b10e97727f5507ecc6b2dbd6cda3
Zane Preudhomme <zane@effarig.com>
2021-09-16 04:55:53 -0700

    Some JS functionality added

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-window.addEventListener("DOM")
+window.addEventListener('DOMContentLoaded', (event) => {
+    
+    
+    let left = document.getElementById("left-arrow")
+    let right = document.getElementById("right-arrow")
+    let carouselName = document.getElementById("carouselName")
+
+    const nameArray= ["app", "blog", "shop", "note", "game", "graveyard"];
+    let position = 0;
+
+    left.addEventListener('click', () => {
+        if (position === 0){
+            position = 5;
+        } else {
+            position--;
+        }
+        carouselName.innerHTML = nameArray[position]
+    })
+
+    right.addEventListener('click', () => {
+        if (position === 5){
+            position = 0;
+        } else {
+            position++;
+        }
+        carouselName.innerHTML = nameArray[position]
+    })
+});
